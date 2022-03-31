fb7d849e8ff41c55d8738406b07c49dc558cfc9f
Luis Miguel Báez Aponte <lmbaeza@unal.edu.co>
2022-03-31 13:08:53 -0500

    sessiones fueron eliminadas

--- content/ordering.ts
+++ content/ordering.ts
@@ -2,11 +2,7 @@
 
 export type SectionID =
   | 'general'
-  | 'bronze'
-  | 'silver'
-  | 'gold'
-  | 'plat'
-  | 'adv';
+  | 'silver';
 
 export type Chapter = {
   name: string;
@@ -67,33 +63,6 @@ const MODULE_ORDERING: { [key in SectionID]: Chapter[] } = {
       items: ['usaco-faq', 'usaco-monthlies', 'usaco-camp', 'resources-usa'],
     },
   ],
-  bronze: [
-    {
-      name: 'Getting Started',
-      items: ['time-comp', 'rect-geo', 'intro-ds', 'simulation'],
-    },
-    {
-      name: 'Complete Search',
-      description:
-        'Solving Bronze problems by checking all possible cases in the solution space.',
-      items: ['intro-complete', 'complete-rec'],
-    },
-    {
-      name: 'Sorting & Sets',
-      description: '',
-      items: ['intro-sorting', 'intro-sets'],
-      // It's not strictly necessary to know any data structures aside from arrays for Bronze, but it definitely helps.
-    },
-    {
-      name: 'Additional',
-      description: 'Topics not strictly necessary to know to pass Bronze.',
-      items: ['ad-hoc', 'intro-greedy', 'intro-graphs'],
-    },
-    {
-      name: 'Conclusion',
-      items: ['bronze-conclusion'],
-    },
-  ],
   silver: [
     {
       name: 'Prefix Sums',
@@ -124,144 +93,7 @@ const MODULE_ORDERING: { [key in SectionID]: Chapter[] } = {
       name: 'Conclusion',
       items: ['silver-conclusion'],
     },
-  ],
-  gold: [
-    {
-      name: 'Introductory Number Theory',
-      items: ['divisibility', 'modular'],
-    },
-    {
-      name: 'Dynamic Programming',
-      description:
-        'Every (?) Gold and Platinum contest has at least one DP problem.',
-      items: [
-        'intro-dp',
-        'knapsack',
-        'paths-grids',
-        'lis',
-        'dp-bitmasks',
-        'dp-ranges',
-      ],
-    },
-    {
-      name: 'Graphs',
-      items: ['bfs', 'dsu', 'toposort', 'shortest-paths', 'mst'],
-    },
-    {
-      name: 'Data Structures',
-      items: ['stacks', 'sliding-window', 'PURS'],
-    },
-    {
-      name: 'Trees',
-      items: ['tree-euler', 'dp-trees', 'all-roots'],
-    },
-    {
-      name: 'Hashing',
-      description: 'Rarely required at this level, but still good to know.',
-      items: ['string-hashing', 'unordered-map', 'faster-hashmap'],
-    },
-    {
-      name: 'Misc. Topics',
-      items: ['intro-bitwise'],
-    },
-    {
-      name: 'Conclusion',
-      items: ['gold-conclusion'],
-    },
-  ],
-  plat: [
-    {
-      name: 'Range Queries',
-      description:
-        'It seems that no Platinum contest is complete without a segment tree ...',
-      items: [
-        'segtree-ext',
-        'range-sweep',
-        'RURQ',
-        'sparse-segtree',
-        '2DRQ',
-        'DC-SRQ',
-        'sqrt',
-      ],
-    },
-    {
-      name: 'Trees',
-      description: '... or a tree!',
-      items: ['binary-jump', 'merging', 'hld', 'centroid'],
-    },
-    {
-      name: 'Geometry',
-      description: 'More advanced concepts in computational geometry.',
-      items: ['geo-pri', 'sweep-line', 'convex-hull', 'convex-hull-trick'],
-    },
-    {
-      name: 'Misc. Topics',
-      items: ['matrix-expo', 'bitsets', 'DC-DP'],
-    },
-    {
-      name: 'Conclusion',
-      items: ['plat-conclusion'],
-    },
-  ],
-  adv: [
-    {
-      name: 'Data Structures',
-      items: [
-        'springboards',
-        'wavelet',
-        'count-min',
-        'segtree-beats',
-        'persistent',
-        'treaps',
-      ],
-    },
-    {
-      name: 'Convexity',
-      items: ['line-container', 'lagrange', 'slope-trick'],
-    },
-    {
-      name: 'Graphs',
-      items: [
-        'sp-neg',
-        'eulerian-tours',
-        'BCC-2CC',
-        'SCC',
-        'offline-del',
-        'eulers-formula',
-        'critical',
-        'link-cut-tree',
-      ],
-    },
-    {
-      name: 'Dynamic Programming',
-      items: ['comb-sub', 'dp-more', 'dp-sos'],
-    },
-    {
-      name: 'Flows',
-      items: ['max-flow', 'min-cut', 'flow-lb', 'min-cost-flow'],
-    },
-    {
-      name: 'Polynomials',
-      items: ['fft', 'fft-ext'],
-    },
-    {
-      name: 'Strings',
-      items: ['string-search', 'suffix-array', 'string-suffix'],
-    },
-    {
-      name: 'Misc. Topics',
-      items: [
-        'extend-euclid',
-        'xor-basis',
-        'fracturing-search',
-        'game-theory',
-        'multiplicative',
-        'matroid-isect',
-        'interactive',
-        'vectorization',
-      ],
-    },
-  ],
+  ]
 };
 
 export default MODULE_ORDERING;
@@ -270,33 +102,17 @@ export const SECTIONS: SectionID[] = Object.keys(
 ) as SectionID[];
 export const SECTION_LABELS: { [key in SectionID]: string } = {
   general: 'General',
-  bronze: 'Bronze',
-  silver: 'Silver',
-  gold: 'Gold',
-  plat: 'Platinum',
-  adv: 'Advanced',
+  silver: 'Silver'
 };
 export const SECTION_SEO_DESCRIPTION: { [key in SectionID]: string } = {
   general:
     'General information for USACO and Competitive Programming. Includes resources to pick a language, set up an IDE, practice tips, and more!',
-  bronze:
-    'Topics for USACO Bronze include time complexity, data structures, sorting, simulation, complete search, ad hoc, greedy, graphs, rectangle geometry, sets and maps, and recursion.',
   silver:
-    'Topics for USACO Silver include binary search on the answer, prefix sums, two pointers, dfs, floodfill, trees, custom comparators, greedy with sorting, and more!',
-  gold:
-    'Topics for USACO Gold include dynamic programming, union-find, shortest paths, point update range sum, topological sort, minimum spanning trees, euler tour, string hashing, and more!',
-  plat:
-    'Topics for USACO Platinum include segment trees, advanced tree techniques, advanced dynamic programming, computational geometry, matrix exponentiation, and more!',
-  adv:
-    'Topics usually outside the scope of most USACO contests include segment tree beats, treaps, slope trick, strongly connected components, max-flow, suffix arrays, fast fourier transform, and more!',
+    'Topics for USACO Silver include binary search on the answer, prefix sums, two pointers, dfs, floodfill, trees, custom comparators, greedy with sorting, and more!'
 };
 export const SECTION_SEO_TITLES: { [key in SectionID]: string } = {
   general: 'General USACO Information',
-  bronze: 'USACO Bronze Topics',
-  silver: 'USACO Silver Topics',
-  gold: 'USACO Gold Topics',
-  plat: 'USACO Platinum Topics',
-  adv: 'Advanced USACO Topics',
+  silver: 'USACO Silver Topics'
 };
 
 const moduleIDToSectionMap: { [key: string]: SectionID } = {};
